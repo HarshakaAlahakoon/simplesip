9d632d8af3da0b93aecfee259ee8d31d88428c5a
HarshakaAlahakoon <harshaka.k.alahakoon@gmail.com>
2017-11-22 11:53:06 +0530

    Added SDP offer parsing functions and records

--- include/simplesip.hrl
+++ include/simplesip.hrl
@@ -27,4 +27,76 @@
   sdp
 }).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%			SDP protocol		%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% TODO:: redefine this record
+-record(sdp_message, {
+	v,  	% (protocol version)
+	o,  	% (owner/creator and session identifier).
+	s,  	% (session name)
+	i = [],	% (session information)
+	u,  	% (URI of description)
+    e,  	% (email address)
+    p,  	% (phone number)
+    c = [], % (connection information - not required if included in all media)
+    b = [], % (bandwidth information)
+    z,  	% (time zone adjustments)
+    k = [], % (encryption key)
+    a = [],	% (zero or more media attribute lines), [{attribute, Value}]
+	t = [],	% (time the session is active)
+    r = [],	% (zero or more repeat times)
+	m = []	% (media name and transport address)
+}).
+
+-record(media, {
+	media,				% media type
+	port,				% transport port to which the media stream is sent
+	protocol,			% transport protocol
+	fmt_list = []		% media format descriptions
+}).
+
+-record(rtpmap, {
+	fmt,
+	encoding,
+	clock_rate,
+	encoding_para = []
+}).
+
+-record(sdp_origin, {
+	username,
+	session_id,
+	version,
+	network_type,
+	address_type,
+	address
+}).
+
+-record(connection_data, {
+	network_type,
+	address_type,
+	connection_address
+}).
+
+-record(encryption_key, {
+	method,
+	key
+}).
+
+-record(attribute, {
+	type,	%% property (flags) / value (media types)
+	flag,
+	attribute,
+	value
+}).
+
+-record(time,{
+	start,
+	stop
+}).
+
+-record(repeat, {
+	repeat_interval,
+	active_duration,
+	offsets_from_start = []
+}).
+
 -endif.
--- src/simplesip_sip_util.erl
+++ src/simplesip_sip_util.erl
@@ -9,34 +9,45 @@ decode(Packet) ->
 	case extract_req_line(lists:nth(1, StrList)) of
     	error ->
       		%% TODO:: define a correct error format
+			io:fwrite("~n~p::~p:: Error~n", [?MODULE, ?LINE]),
       		error;
     	SipReq ->
-      		extract_header(SipReq, lists:delete(lists:nth(1, StrList), StrList))
+      		SipReqNew = extract_header(SipReq, lists:nthtail(1, StrList)),
+			case SipReqNew#sip_message.'content-length' of
+				Length when is_integer(Length) and (Length > 0) ->
+					Start = erlang:byte_size(Packet) - Length,
+					SdpBin = erlang:binary_part(Packet, {Start, Length}),
+					%% TODO:: handle error and create #sip_message{}
+					SdpMsgStr = binary:bin_to_list(SdpBin),
+					SdpStrList = string:tokens(SdpMsgStr, "\r\n"),
+					% io:fwrite("~n~p::~p:: Message body : ~p~n", [?MODULE, ?LINE, SdpStrList]),
+					SdpRec = extract_sdp(#sdp_message{}, SdpStrList),
+					% io:fwrite("~n~p~n~p~n", [SipReqNew, SdpRec]);
+					SdpRec;
+				_ ->
+					io:fwrite("~n~p::~p:: Empty message body~n", [?MODULE, ?LINE]),
+					ok
+			end
   	end.
 
 %% determine the received message is whether SIP, and if it is SIP extract the request type or status code.
 extract_req_line(Str) ->
-	% StrList = string:tokens(Str, " "),
 	[Var1, Var2, Var3] = string:tokens(Str, " "),
- %  	Fields = case string:str(lists:last(StrList), "SIP") of
   	Fields = case string:str(Var3, "SIP") of
     	0 ->
-      % 		case string:str(lists:nth(1, StrList), "SIP") of
       		case string:str(Var1, "SIP") of
         		0 ->
           			error;
         		_ ->
-          	% 		{Code, _} = string:to_integer(lists:nth(2, StrList)),
           			{Code, _} = string:to_integer(Var2),
           			[{type, status}, {status, Code}]
       		end;
     	_ ->
-      % 		[Method, ReqUri, _] = StrList,
       		Method = Var1,
       		MethodLow = string:to_lower(Method),
       		try erlang:list_to_existing_atom(MethodLow) of
         		MethodAtom ->
-          			[{type, method},{method, MethodAtom}]
+          			[{type, method}, {method, MethodAtom}]
       		catch
         		_:_ ->
           			MethodAtom = erlang:list_to_atom(MethodLow),
@@ -58,11 +69,10 @@ extract_req_line(Str) ->
 
 extract_header(SipRec, []) ->
   	SipRec;
-extract_header(SipRec, StrList) ->
-  	Line = lists:nth(1, StrList),
+extract_header(SipRec, [Line | Rest]) ->
   	case string:chr(Line, $:) of
     	0 ->
-      		extract_header(SipRec, lists:delete(Line, StrList));
+      		extract_header(SipRec, Rest);
     	Position ->
       		Field = string:sub_string(Line, 1, Position-1),
       		% io:fwrite("~nField : ~p~n", [Field]),
@@ -126,5 +136,136 @@ extract_header(SipRec, StrList) ->
           		_ ->
             		SipRec
     	end,
-    	extract_header(NewSipRec, lists:delete(Line, StrList))
+    	extract_header(NewSipRec, Rest)
+	end.
+
+extract_sdp(SdpRec, []) ->
+	SdpRec;
+extract_sdp(SdpRec, [Line | Rest]) ->
+	%% NOTE:: match first charactor of the line
+	Str = string:substr(Line, 3),
+	% io:fwrite("~n~p::~p:: Line : ~p~n", [?MODULE, ?LINE, Str]),
+	NewSdpRec = case string_to_atom(string:substr(Line, 1, 1)) of
+		v ->
+			SdpRec#sdp_message{v = Str};
+		o ->
+			[Uname, SessId, V, NetType, AddType, Addr] = string:tokens(Str, " "),
+			SdpOri = #sdp_origin{
+				username = Uname,
+				session_id = SessId,
+				version = V,
+				network_type = NetType,
+				address_type = AddType,
+				address = Addr
+			},
+			SdpRec#sdp_message{o = SdpOri};
+		s ->
+			SdpRec#sdp_message{s = Str};
+		i ->
+			%% TODO:: how to store and use this?
+			SdpRec#sdp_message{i = lists:append(SdpRec#sdp_message.i, [Str])};
+		u ->
+			%% TODO:: how to store and use this?
+			SdpRec#sdp_message{u = Str};
+		e ->
+			SdpRec#sdp_message{e = Str};
+		p ->
+			SdpRec#sdp_message{p = Str};
+		c ->
+			TokenList = string:tokens(Str, " "),
+			ConnData = #connection_data{
+				network_type = lists:nth(1, TokenList),
+				address_type = lists:nth(2, TokenList),
+				connection_address = lists:nth(3, TokenList)
+			},
+			SdpRec#sdp_message{c = lists:append(SdpRec#sdp_message.c, [ConnData])};
+		b ->
+			%% TODO:: how to store and use this?
+			SdpRec#sdp_message{b = lists:append(SdpRec#sdp_message.b, [Str])};
+		z ->
+			%% TODO:: how to store and use this?
+			SdpRec#sdp_message{z = Str};
+		k ->
+			%% TODO:: how to store and use this?
+			% EncKey = #encryption_key{
+			% 	method,
+			% 	key
+			% },
+			SdpRec#sdp_message{k = lists:append(SdpRec#sdp_message.k, [Str])};
+		a ->
+			SdpRec#sdp_message{a = lists:append(SdpRec#sdp_message.a, [extract_attribute(Str)])};
+		t ->
+			%% TODO:: how to store and use this?
+			[StartStr, StopStr] = string:tokens(Str, " "),
+			{Start, _} = string:to_integer(StartStr),
+			{Stop, _} = string:to_integer(StopStr),
+			TimeRec = #time{
+				start = Start,
+				stop = Stop
+			},
+			SdpRec#sdp_message{t = lists:append(SdpRec#sdp_message.t, [TimeRec])};
+		r ->
+			%% TODO:: how to store and use this?
+			TokenList = string:tokens(Str, " "),
+			RepeatRec = #repeat{
+				repeat_interval = lists:nth(1, TokenList),
+				active_duration = lists:nth(2, TokenList),
+				offsets_from_start = lists:nthtail(2, TokenList)
+			},
+			SdpRec#sdp_message{r = lists:append(SdpRec#sdp_message.r, [RepeatRec])};
+		m ->
+			%% TODO:: how to store and use this?
+			TokenList = string:tokens(Str, " "),
+			MediaRec = #media{
+				media = lists:nth(1, TokenList),
+				port = lists:nth(2, TokenList),
+				protocol = lists:nth(3, TokenList),
+				fmt_list = lists:nthtail(3, TokenList)
+			},
+			SdpRec#sdp_message{m = lists:append(SdpRec#sdp_message.m, [MediaRec])};
+		_ ->
+			SdpRec
+	end,
+	extract_sdp(NewSdpRec, Rest).
+
+extract_attribute(Str) ->
+	case string:tokens(Str, ":") of
+		[Flag] ->
+			#attribute{
+				type = property,
+				flag = Flag
+			};
+		[Attr, Value] ->
+			case string_to_atom(Attr) of
+				NewAttr when NewAttr==rtpmap ->
+					case string:tokens(Value, " /") of
+						[Fmt, Enc, Clk] ->
+							Para = [];
+						[Fmt, Enc, Clk, Para] ->
+							ok
+					end,
+					NewValue = #rtpmap{
+						fmt = Fmt,
+						encoding = Enc,
+						clock_rate = Clk,
+						encoding_para = Para
+					},
+					ok;
+				NewAttr ->
+					NewValue = Value
+			end,
+			#attribute{
+				type = value,
+				attribute = NewAttr,
+				value = NewValue
+			}
+	end.
+
+string_to_atom(Str) ->
+	try erlang:list_to_existing_atom(Str) of
+		Atom ->
+			Atom
+	catch
+		_:_ ->
+			erlang:list_to_atom(Str)
 	end.
